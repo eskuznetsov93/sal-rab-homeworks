dec0cea61d8e8dfab330cbf0afd1b76fa8a075b6
Evgenii Kuznetsov <eskuznetsov93@gmail.com>
2023-11-06 23:45:34 +0500

    Task_final_2

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,15 +34,15 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-let data = {goods: [], order: {}};
-let countOfGoods = goods.length;
+    let data = {goods: [], order: {}};
+    let countOfGoods = goods.length;
 
-for (let i = 0; i < countOfGoods; i++) {
-    data.goods.push({title:goods[i].title, count:goods[i].count});
-}
-data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
-data.order.sum = sum;
-data.client = name + " " + phone;
-let jsonData = JSON.stringify({data:data});
-return jsonData;
+    for (let i = 0; i < countOfGoods; i++) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
+    }
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв. " + address.flat;
+    data.order.sum = sum;
+    data.client = name + " " + phone;
+    let jsonData = JSON.stringify({data:data});
+    return jsonData;
 }
